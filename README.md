644369e8819b9d9e3ab8b2ea08728fb0df92a5bd
Thomas Gredig <thomas.gredig@csulb.edu>
2019-11-19 22:16:53 -0800

    add CSU LA, Northridge, and UC Irvine

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 Yearly distribution of publications from CSU Long Beach
 
 ![Publications per year for CSU Long Beach](images/CSULB-pubs.png)
+
+The same statistics for CSU Los Angeles
+
+![Publications per year for CSU Los Angeles](images/CSULA-pubs.png)
+
+The same statistics for CSU Northridge
+
+![Publications per year for CSU Northridge](images/CSUNorthridge-pubs.png)
+
+The same statistics for UC Irvine
+
+![Publications per year for UC Irvine](images/UCIrvine-pubs.png)
--- publications-per-year.R
+++ publications-per-year.R
@@ -4,6 +4,7 @@ path.FIGS = 'images/'
 
 dir(path.RAW)
 
+## CSU Long Beach
 d = read.csv(file.path(path.RAW,'pub-zp90840-year.txt'),sep='\t')
 d= na.omit(d)
 head(d)
@@ -13,6 +14,56 @@ ggplot(d, aes(Publication.Years, records)) +
   geom_histogram(stat='identity', fill='orange', col='black') +
   xlab('year') +
   theme_bw() +
+  scale_y_continuous(limits=c(0,450)) + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 ggsave(file.path(path.FIGS,'CSULB-pubs.png'),
        width=6,height=5,dpi=150)
+
+## CSU Los Angeles
+d = read.csv(file.path(path.RAW,'pub-zp90032-year.txt'),sep='\t')
+d= na.omit(d)
+head(d)
+names(d)
+
+ggplot(d[-1,], aes(Publication.Years, records)) + 
+  geom_histogram(stat='identity', fill='orange', col='black') +
+  xlab('year') +
+  scale_y_continuous(limits=c(0,450)) + 
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(file.path(path.FIGS,'CSULA-pubs.png'),
+       width=6,height=5,dpi=150)
+
+
+## CSU Northridge
+d = read.csv(file.path(path.RAW,'pub-zp91330-year.txt'),sep='\t')
+d= na.omit(d)
+head(d)
+names(d)
+
+ggplot(d, aes(Publication.Years, records)) + 
+  geom_histogram(stat='identity', fill='orange', col='black') +
+  xlab('year') +
+  scale_y_continuous(limits=c(0,450)) + 
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(file.path(path.FIGS,'CSUNorthridge-pubs.png'),
+       width=6,height=5,dpi=150)
+
+
+## UC Irvine
+d = read.csv(file.path(path.RAW,'pub-zp92697-year.txt'),sep='\t')
+d= na.omit(d)
+head(d)
+names(d)
+
+ggplot(d[-1,], aes(Publication.Years, records)) + 
+  geom_histogram(stat='identity', fill='orange', col='black') +
+  xlab('year') +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(file.path(path.FIGS,'UCIrvine-pubs.png'),
+       width=6,height=5,dpi=150)
+
+
+
